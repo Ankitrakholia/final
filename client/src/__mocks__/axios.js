40151736cc45b7239540539c96187ecc7a0674ed
Ankit Rakholia <ankitrakholia20@gnu.ac.in>
2024-05-09 21:55:44 +0530

    final final

--- client/src/__mocks__/axios.js
+++ client/src/__mocks__/axios.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/api/auth/";
+const API_URL = "https://final-nine-chi.vercel.app/api/auth/";
 
 export default {
     get: jest.fn(() => Promise.resolve({ data: {} })),
